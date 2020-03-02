3b1d68b5c7ec1c3a22d1de4a4eb2c44f7aa70047
Veronica Andrade <veronica726@gmail.com>
2020-03-02 16:58:09 -0700

    add test

--- src/MainPage/MainPage.test.js
+++ src/MainPage/MainPage.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { MainPage } from './MainPage';
+import { MainPage, mapStateToProps, mapDispatchToProps } from './MainPage';
+import { setJoke } from '../Actions'
 import { shallow } from 'enzyme';
 
 describe('MainPage', () => {
@@ -21,3 +22,49 @@ describe('MainPage', () => {
     expect(wrapper).toMatchSnapshot();
   });
 }); 
+
+describe('mapStateToProps', () => {
+  it('should return a joke object', () => {
+    const mockState = {
+      joke: {
+        id: 42,
+        setup: 'how much does a polarbear weigh?',
+        delivery: 'enough to bresk the ice'
+      },
+      fetchParams: {
+        category: 'Any',
+        blacklistFlags: [],
+        type: []
+      }
+    }
+    const expected = {
+        joke: {
+          id: 42,
+          setup: 'how much does a polarbear weigh?',
+          delivery: 'enough to bresk the ice'
+        },
+        fetchParams: {
+          category: 'Any',
+          blacklistFlags: [],
+          type: []
+        }
+    }
+    const mappedProps = mapStateToProps(mockState)
+    expect(mappedProps).toEqual(expected)
+  });
+});
+
+describe('mapDispatchToProps', () => {
+  it('should call dispatch with setJoke', () => {
+    const mockDispatch = jest.fn();
+    const mockJoke = {
+      id: 42,
+      setup: 'how much does a polarbear weigh?',
+      delivery: 'enough to bresk the ice'
+    }
+    const actionToDispatch = setJoke(mockJoke);
+    const mappedProps = mapDispatchToProps(mockDispatch);
+    mappedProps.updateJoke(mockJoke);
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
+  });
+})
