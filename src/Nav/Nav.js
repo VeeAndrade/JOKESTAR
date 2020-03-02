75faf52e591c1f1840ed4fabcae7d1d712a02f04
Veronica Andrade <veronica726@gmail.com>
2020-03-01 19:56:39 -0700

    Add a little bit of logic to the nav bar but still needs some tweaking

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+// import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 import './Nav.scss';
 
-const Nav = () => {
+const Nav = (props) => {
+  const username = useSelector(state => state.username)
   return (
     <nav className='main-nav'>
-      <h1 className='nav-title'>🌟JOKE<span>STAR</span></h1>
-      <NavLink to='/login'>
-        <button className='nav-login-button'>Login</button>
-        </NavLink>
-        
+      <h1 className='nav-title'><span role='img' aria-label='icon'>🌟</span>JOKE<span>STAR</span></h1>
+        {!username ?
+        <NavLink to='/login'><button className='nav-login-button'>Login</button></NavLink> : 
+        <NavLink to='/'><button className='nav-login-button'>Logout</button></NavLink>}
     </nav>
   )
 }
+
+
 
 export default Nav;
