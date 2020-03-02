092b22008b6f22450239e2b710a8f939527f226e
Veronica Andrade <veronica726@gmail.com>
2020-03-01 19:55:40 -0700

    Add main page component with Dom manipulation logic

---
+++ src/MainPage/MainPage.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import JokeForm from '../JokeForm/JokeForm';
+import SafeJoke from '../SafeJoke/SafeJoke'
+import NSFWJoke from '../NSFWJoke/NSFWJoke';
+import Favorites from '../Favorites/Favorites';
+import { getRandomJoke } from '../apiCalls';
+import { getSpecificJoke } from '../apiCalls';
+import { setJoke } from '../Actions';
+import { favoriteJoke } from '../Actions';
+import { connect } from 'react-redux';
+import './MainPage.scss';
+
+class MainPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      clickedButton: ''
+    }
+  }
+
+  setClickedButton = (e) => {
+    this.setState({clickedButton: e.target.value})
+  }
+
+  determinePageDisplay = () => {
+    switch(this.state.clickedButton) {
+      case 'nsfw':
+        return <JokeForm findClickedBtn={this.setClickedButton}/>
+      case 'safejoke':
+        return <SafeJoke grabJoke={this.grabRandomJoke} favoriteJoke={this.addJokeToFavorites}/>
+      case 'getJoke':
+        return <NSFWJoke joke={this.props.joke} getNextJoke={this.grabNSFWJoke} favoriteJoke={this.addJokeToFavorites}/>
+      case 'favorites':
+        return <Favorites />
+      default:
+        return <h3>Welcome to JOKESTAR!</h3>
+    }
+  }
+
+  grabRandomJoke = async () => {
+    let promiseJoke = await getRandomJoke()
+    this.props.updateJoke(promiseJoke)
+    return promiseJoke;
+  }
+
+  addJokeToFavorites = (e) => {
+    this.props.favoriteJoke(this.props.joke)
+  }
+
+  grabNSFWJoke = async () => {
+    let promiseJoke = await getSpecificJoke(this.state)
+    this.props.updateJoke(promiseJoke)
+    return promiseJoke
+  }
+
+  render() {
+    return (
+      <section className='users-main-page'>
+        <aside className='user-features-section'>
+          <button className='feature-button' value='safejoke' onClick={(event) => this.setClickedButton(event)}>Safe Jokes</button>
+          <button className='feature-button' value='nsfw' onClick={(event) => this.setClickedButton(event)}>NSFW</button>
+          <button className='feature-button' value='favorites' onClick={(event) => this.setClickedButton(event)}>Favorites</button>
+          <button className='feature-button' value='clickMe' onClick={(event) => this.setClickedButton(event)}>Click Me</button>
+        </aside>
+        <section className='display-content-section'>
+          {this.determinePageDisplay()}
+        </section>
+      </section>
+    )
+  }
+}
+
+export const mapStateToProps = state => ({
+  joke: state.joke
+})
+
+export const mapDispatchToProps = dispatch => ({
+  updateJoke: joke => dispatch(setJoke(joke)),
+  favoriteJoke: joke => dispatch(favoriteJoke(joke)) 
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainPage);
