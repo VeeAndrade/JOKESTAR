357bead0c6d2411a76633683d05eddcf85089a11
Veronica Andrade <veronica726@gmail.com>
2020-03-01 19:27:09 -0700

    Implement redux by creating store

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { rootReducer } from './Reducers'
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(rootReducer, composeWithDevTools());
+
 const router = (
-  <BrowserRouter>
-  <App />
-</BrowserRouter>
-)
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
 
 ReactDOM.render(router, document.getElementById('root'));
 
